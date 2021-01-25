260ee43f331247e3c6f276e1541e2634d0ac1121
xiexiaoyu <xiexiaoyu@piesat.cn>
2021-01-25 11:07:13 +0800

    vueObject

---
+++ src/components/homePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="homePage">
+    <div>{{$store.state.test}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'homePage',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#homePage{
+  height: 100vh;
+  width: 100%;
+}
+</style>
